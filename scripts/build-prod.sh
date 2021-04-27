dddae29cfb4a3ffc03978338dd08f6bf287a52b9
Argana Phangquestian <arganaphang@gmail.com>
2021-04-28 00:42:41 +0700

    Change Domain

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.example.org)
+domains=(api-lms.falcer.tech)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="falcerdeveloper@gmail.com" # Adding a valid address is strongly recommended
